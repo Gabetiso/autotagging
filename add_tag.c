06ad6dc8f36268049855697dfbd7f381881ea841
Gabetiso <gabetiso@gmail.com>
2018-08-08 00:18:28 +0900

    綺麗にした

--- add_tag.c
+++ add_tag.c
@@ -12,9 +12,8 @@ void Hstatus();
 void write();
 
 
-int i=0;
-int j=0;
-int M=250;//fgetsで読み取れる文字数
+int L=5000;//作品の本文のSptextあるいはSpStage内の最大文字数
+int M=250;//タグ情報の最大文字数
 int N=4000;//エクセルの行数(3000あれば足りそう)
 char text[5000];
 char Spe[4000][100];
@@ -25,7 +24,6 @@ char Sge[4000][100];
 char Hge[4000][100];
 char Sst[4000][100];
 char Hst[4000][100];
-char x[50][3000];//配列長さ合計に限界あるっぽい
 
 
 
@@ -177,10 +175,11 @@ void Hstatus(){
 
 //記述部分
 void write(){
-  i=0;
-  j=0;
+  int i=0;
+  int j=0;
   FILE *fr;
   FILE *fs;
+  char x[50][3000];//配列長さ合計に限界あるので調整注意
   char *p;
 
   //Writing.txtは出力ファイル名になる
@@ -190,63 +189,63 @@ void write(){
     printf("Can't open\n");
   }
   //一行ずつ読み出しtext配列に保存していく。
-  while(fgets(text, 5000, fs) != NULL){
+  while(fgets(text, L, fs) != NULL){
     strtok(text,"\r\n");
 
-    if(strcmp(Spe[i],"StD")==0){
+    //Oxygen用に成型していく
+    if(strcmp(Spe[i],"StD")==0){//Spe配列と"StD"という文字列を、strcmpで比較する。イコールならば0をかえす
       fprintf(fr, "<p><stage>%s</stage></p>\n",text);
-    }else if(strcmp(Spe[i],"\0")==0){//終了した場合
-      break;
+    }else if(strcmp(Spe[i],"\0")==0){
+      break;//終了した場合は抜ける(文末には\0がある)
     }else{
       fprintf(fr, "<sp speaker=\"%s\" hearer=\"%s\" Srelation=\"%s\" Hrelation=\"%s\" Sgender=\"%s\" Hgender=\"%s\" Sstatus=\"%s\" Hstatus=\"%s\">\n   <speaker>%s</speaker>\n   <p>\n",Spe[i], Hea[i], Sre[i], Hre[i], Sge[i], Hge[i], Sst[i], Hst[i], Spe[i]);
-      // ()を区切りに文字列を分割
+
+      // ()を区切りに文字列を分割(SptextとSpstageに分ける)
       // 1回目
-      p = strtok(text, "()");
-      strcpy(x[j],p);
+      p = strtok(text, "()");//"("あるいは")"を区切りにしてtext配列内の文字列を分割する。区切り文字はNULLに変更される。
+      strcpy(x[j],p);//pに格納された文字列をx配列にコピーする
       // 2回目以降
       while(p != NULL) {
-	j++;
-	// strtok関数により変更されたNULLのポインタが先頭
-	p = strtok(NULL, "()");
-	if(p!=NULL){
-	  strcpy(x[j],p);
-	}else{
-	  strcpy(x[j],"^");
-	}
+	       j++;
+	       // strtokによって変更されたNULLポインタが先頭
+	       p = strtok(NULL, "()");
+         if(p!=NULL){
+	         strcpy(x[j],p);
+	       }else{
+	         strcpy(x[j],"^");//分割が終わった地点のx[j]に"^"という楔を打つ
+	       }
       }
       j=0;
       //ここまで分割処理
 
-
       if(strncmp(text, "(", 1) == 0){
-	while(strcmp(x[j] , "^") != 0){  //文字列比較注意
-	  if(strcmp(x[j]," ") != 0 && strcmp(x[j],"  ")){//空白の処理
-	    fprintf(fr,"    <Spstage>(%s)</Spstage>\n",x[j]);
-	  }
-	  j++;
-	  if(strcmp(x[j] , "^") != 0 && strcmp(x[j]," ") != 0 && strcmp(x[j],"  ")){
-	    fprintf(fr,"     <Sptext>%s</Sptext>\n",x[j]);
-	    j++;
-	  }
-	}
-      }else{
-	while(strcmp(x[j] , "^") != 0){
-	  if(strcmp(x[j]," ") != 0 && strcmp(x[j],"  ")){
-	    fprintf(fr,"    <Sptext>%s</Sptext>\n",x[j]);
-	  }
-	  j++;
-	  if(strcmp(x[j] , "^") != 0 && strcmp(x[j]," ") != 0 && strcmp(x[j],"  ")){
-	    fprintf(fr,"    <Spstage>(%s)</Spstage>\n",x[j]);
-	    j++;
-	  }
-	}
-      }
-
-      fprintf(fr,"   </p>\n</sp>\n");
-    }//elseの閉じ括弧
-    i++;
-    j=0;
-  }
-  fclose(fs);
-  fclose(fr);
+        while(strcmp(x[j] , "^") != 0){  //文字列比較注意
+	         if(strcmp(x[j]," ") != 0 && strcmp(x[j],"  ")){//空白の処理
+	            fprintf(fr,"    <Spstage>(%s)</Spstage>\n",x[j]);
+	         }
+           j++;
+           if(strcmp(x[j] , "^") != 0 && strcmp(x[j]," ") != 0 && strcmp(x[j],"  ")){
+             fprintf(fr,"     <Sptext>%s</Sptext>\n",x[j]);
+	           j++;
+	         }
+         }
+       }else{
+         while(strcmp(x[j] , "^") != 0){
+           if(strcmp(x[j]," ") != 0 && strcmp(x[j],"  ")){
+             fprintf(fr,"    <Sptext>%s</Sptext>\n",x[j]);
+           }
+           j++;
+           if(strcmp(x[j] , "^") != 0 && strcmp(x[j]," ") != 0 && strcmp(x[j],"  ")){
+             fprintf(fr,"    <Spstage>(%s)</Spstage>\n",x[j]);
+             j++;
+           }
+         }
+       }
+       fprintf(fr,"   </p>\n</sp>\n");
+     }//elseの閉じ括弧
+     i++;
+     j=0;
+   }
+   fclose(fs);
+   fclose(fr);
 }
